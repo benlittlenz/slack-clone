aca0bcf08b627cabf5348cfb4d6f565a55e85777
nzbenji <benlittle53@gmail.com>
2019-06-14 20:26:58 +1200

    Retrieving register data info

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,13 +1,42 @@
 import React from 'react';
+import firebase from '../firebase';
 import { Grid, Form, Segment, Button, Header, Message, Icon } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 class Register extends React.Component {
-
-    handleChange = () => {
-
+    state = {
+        username: '',
+        email: '',
+        password: '',
+        passwordConfirmation: ''
     }
+
+
+
+    handleChange = event => {
+        this.setState({
+            [event.target.name] : event.target.value
+        })
+    }
+
+    handleSubmit = async event => {
+        if(this.isFormValid());
+        event.preventDefault();
+        const data = await firebase
+                        .auth()
+                        .createUserWithEmailAndPassword(this.state.email, this.state.password)
+
+            console.log(data);
+            // .then(createdUser => {
+            //     console.log(createdUser)
+            // })
+            // .catch(err => {
+            //     console.error(err);
+            // })
+    }
+
     render() {
+        const {username, email, password, passwordConfirmation} = this.state;
         return (
             <Grid textAlign="center" verticalAlign="middle" className="app">
                 <Grid.Column style={{ maxWidth: 450 }}>
@@ -15,19 +44,19 @@ class Register extends React.Component {
                         <Icon name="puzzle piece" color="orange" />
                         Register for DevChat
                     </Header>
-                    <Form size="large">
+                    <Form onSubmit={this.handleSubmit} size="large">
                         <Segment stacked>
                             <Form.Input fluid name="username" icon="user" iconPosition="left"
-                            placeholder="Username" onChange={this.handleChange} type="text" />
+                            placeholder="Username" value={username} onChange={this.handleChange} type="text" />
 
                             <Form.Input fluid name="email" icon="mail" iconPosition="left"
-                            placeholder="Email Address" onChange={this.handleChange} type="email" />
+                            placeholder="Email Address" value={email} onChange={this.handleChange} type="email" />
 
                             <Form.Input fluid name="password" icon="lock" iconPosition="left"
-                            placeholder="Password" onChange={this.handleChange} type="password" />
+                            placeholder="Password" value={password} onChange={this.handleChange} type="password" />
 
                             <Form.Input fluid name="passwordConfirmation" icon="repeat" iconPosition="left"
-                            placeholder="Password Confirmation" onChange={this.handleChange} type="password" />
+                            placeholder="Password Confirmation" value={passwordConfirmation} onChange={this.handleChange} type="password" />
 
                             <Button color="orange" fluid size="large">Submit</Button> 
                         </Segment>
