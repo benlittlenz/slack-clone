df9d990c68a1b8b965935aa6ac7256b0f7b2e9ab
nzbenji <benlittle53@gmail.com>
2019-06-14 18:34:02 +1200

    Register form and styling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import 'semantic-ui-css/semantic.min.css'
+
 import App from './components/App';
 import Login from './components/Login';
 import Register from './components/Register';
