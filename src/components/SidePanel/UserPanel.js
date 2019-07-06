65156df3c8138648fbc75bfcfe87e549dd0ae065
nzbenji <benlittle53@gmail.com>
2019-07-06 15:27:54 +1200

    added avatar for authenticated user

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Grid, Header, Icon, Dropdown } from 'semantic-ui-react';
+import { Grid, Header, Icon, Dropdown, Image } from 'semantic-ui-react';
 import firebase from '../../firebase';
 import { sign } from 'crypto';
 
@@ -33,6 +33,7 @@ class UserPanel extends React.Component {
     }
 
     render() {
+        const { user } = this.state;
         return (
             <Grid style={{background: '#4c3c4c' }}>
                 <Grid.Column>
@@ -43,12 +44,17 @@ class UserPanel extends React.Component {
                                 DevChat
                             </Header.Content>
                         </Header>
-                    </Grid.Row>
-                    <Header style={{ padding: '0.25em'}} as="h4" inverted>
+                        <Header style={{ padding: '0.25em'}} as="h4" inverted>
                         <Dropdown
-                        trigger={<span>{this.state.user.displayName}</span>} 
+                        trigger={
+                            <span>
+                                <Image src={user.photoURL} spaced="right" avatar />
+                                {user.displayName}
+                            </span>
+                        } 
                         options={this.dropDownOptions()} />
                     </Header>
+                    </Grid.Row>
                 </Grid.Column>
             </Grid>
         )
